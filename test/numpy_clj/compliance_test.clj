70a26e979b9e477d5876c526ecd65cbb6a44cbfc
Brian Chevalier <>
2020-12-04 11:29:18 -0700

    Added tests to comment block for easier testing

--- test/numpy_clj/compliance_test.clj
+++ test/numpy_clj/compliance_test.clj
@@ -1,11 +1,35 @@
 (ns numpy-clj.compliance-test
   (:require [numpy-clj.core :as core]
             [clojure.test :refer [deftest run-tests]];;is testing
-            [clojure.core.matrix.compliance-tester :as core-test]))
+            [clojure.core.matrix :as mat]
+            [clojure.core.matrix.compliance-tester :as t]))
 
-(deftest compliance-test
-  (core-test/compliance-test :numpy-clj))
+;; (deftest compliance-test
+;;   (t/compliance-test :numpy-clj))
 
 (defn -main []
-  (core-test/compliance-test :numpy-clj)
+  (t/compliance-test :numpy-clj)
   #_(run-tests))
+
+(comment
+  (def im core/cannonical-object)
+  (t/instance-test im)
+  
+  ;;(t/test-implementation im)
+  (mat/new-scalar-array im)
+  (t/test-impl-scalar-array im)
+  (t/test-implementation-key im)
+  (t/test-assumptions-for-all-sizes im)
+  (t/test-coerce-via-vectors im)
+  (t/test-equality im)
+  (t/test-methods-existence im)
+  (when (mat/supports-dimensionality? im 2)
+    (t/matrix-tests-2d im))
+  (when (mat/supports-dimensionality? im 1)
+    (t/vector-tests-1d im))
+  (t/test-array-interop im)
+  (t/test-numeric-functions im)
+  (t/test-dimensionality im)
+  (t/test-row-operations im)
+  (t/test-qr im)
+  (t/test-new-matrices im))
