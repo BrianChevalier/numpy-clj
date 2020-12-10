3b14d91dd78cbc3e4f1b328e885a8b830b69988e
Brian Chevalier <>
2020-12-09 23:54:22 -0700

    Removed unnecessary dependencies

--- test/numpy_clj/compliance_test.clj
+++ test/numpy_clj/compliance_test.clj
@@ -1,19 +1,16 @@
 (ns numpy-clj.compliance-test
-  (:require [numpy-clj.core :as core]
-            [clojure.test :refer [deftest is run-tests testing]];;is testing
-            [clojure.core.matrix :as mat]
-            [numpy-clj.operators :as op]
-            [libpython-clj.python :refer [py. py.. py.-] :as py]
-            [clojure.core.matrix.compliance-tester :as t]
-            [libpython-clj.require :refer [require-python]]))
+  (:require numpy-clj.environment))
 
-(require-python '[numpy :as np])
-(require-python '[builtins])
-(require-python '[numpy.linalg :as lin])
-(require-python '[scipy.linalg :as scilin])
+(numpy-clj.environment/main!)
+
+(require '[numpy-clj.core :as core]
+         '[clojure.test :refer [deftest is run-tests testing]];;is testing
+         '[clojure.core.matrix.compliance-tester :as t])
 
 (deftest compliance-test
-   (t/compliance-test :numpy-clj))
+  (t/compliance-test :numpy-clj))
 
 (defn -main []
+  (numpy-clj.environment/main!)
+  #_(run-tests)
   (t/compliance-test :numpy-clj))
